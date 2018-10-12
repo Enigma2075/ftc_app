73f7d6c11de9f8f99631c7603dd7218b4f9a23bb
noshownoah <43425203+noshownoah@users.noreply.github.com>
2018-10-12 19:33:40 -0400

    worked on teleop to get it runing

--- Team5391/src/main/java/org/firstinspires/ftc/team5391/Teleop.java
+++ Team5391/src/main/java/org/firstinspires/ftc/team5391/Teleop.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.team5391;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.robotcontroller.external.samples.BasicOpMode_Iterative;
-
+@TeleOp(name= "TeleOp drive", group= "learn TeleOp"  )
 public class Teleop extends OpMode {
 
     HardwareDrivetrain drivetrain = null;
@@ -21,7 +23,7 @@ public class Teleop extends OpMode {
 
     @Override
     public void loop() {
-        DriveSignal powers = cheesyDrive.cheesyDrive(gamepad1.left_stick_y, gamepad1.right_stick_x, false);
+        DriveSignal powers = cheesyDrive.cheesyDrive(gamepad1.left_stick_y*-1, gamepad1.right_stick_x, false);
         drivetrain.setPower(powers.rightMotor, powers.leftMotor);
     }
 }
