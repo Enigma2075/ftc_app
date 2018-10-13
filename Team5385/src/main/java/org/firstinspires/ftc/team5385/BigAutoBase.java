3552c6cf82b2a2005e1fce292e3c31f94e21a951
ELOmrbluesky <43425166+ELOmrbluesky@users.noreply.github.com>
2018-10-13 11:27:16 -0400

    Update ratio

--- Team5385/src/main/java/org/firstinspires/ftc/team5385/BigAutoBase.java
+++ Team5385/src/main/java/org/firstinspires/ftc/team5385/BigAutoBase.java
@@ -12,7 +12,7 @@ public class BigAutoBase  extends LinearOpMode {
     AdafruitBNO055IMU gyro = null;                    // Additional Gyro device
 
     static final double COUNTS_PER_MOTOR_REV = 1120;    // eg: TETRIX Motor Encoder
-    static final double DRIVE_GEAR_REDUCTION = 1.0;     // This is < 1.0 if geared UP
+    static final double DRIVE_GEAR_REDUCTION = .75;     // This is < 1.0 if geared UP
     static final double WHEEL_DIAMETER_INCHES = 4.0;     // For figuring circumference
     static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
             (WHEEL_DIAMETER_INCHES * 3.1415);
