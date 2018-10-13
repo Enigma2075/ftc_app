08a2029bfe29ce6c4d9e929d47790613bd3b2fbf
Abubakar <masiddiq05@gmail.com>
2018-10-13 10:04:18 -0400

    Changed OpModes in Teleop
    I changed it so that the Teleop class was connected to the BaseAutonomous and I also changed the OpModes so that instead of init, it was runOpMode, and instead of loop, I said while(OpModeIsActive)(){}

--- Team5391/src/main/java/org/firstinspires/ftc/team5391/Teleop.java
+++ Team5391/src/main/java/org/firstinspires/ftc/team5391/Teleop.java
@@ -7,23 +7,20 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.robotcontroller.external.samples.BasicOpMode_Iterative;
 @TeleOp(name= "TeleOp drive", group= "learn TeleOp"  )
-public class Teleop extends OpMode {
+public class Teleop extends BaseAutonomous {
 
     HardwareDrivetrain drivetrain = null;
     CheesyDrive cheesyDrive = null;
 
     @Override
-    public void init() {
+    public void runOpMode() {
         drivetrain = new HardwareDrivetrain();
         drivetrain.init(hardwareMap);
         cheesyDrive = new CheesyDrive();
         drivetrain.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-    }
-
-
-    @Override
-    public void loop() {
-        DriveSignal powers = cheesyDrive.cheesyDrive(gamepad1.left_stick_y*-1, gamepad1.right_stick_x, false);
-        drivetrain.setPower(powers.rightMotor, powers.leftMotor);
+        while (opModeIsActive()) {
+            DriveSignal powers = cheesyDrive.cheesyDrive(gamepad1.left_stick_y * -1, gamepad1.right_stick_x, false);
+            drivetrain.setPower(powers.rightMotor, powers.leftMotor);
+        }
     }
 }
