ac89e5cd5047dcdea7b63aa847adf25486d5d3f9
ELOmrbluesky <43425166+ELOmrbluesky@users.noreply.github.com>
2018-10-13 11:13:34 -0400

    made it move

--- Team5385/src/main/java/org/firstinspires/ftc/team5385/Tele_Op.java
+++ Team5385/src/main/java/org/firstinspires/ftc/team5385/Tele_Op.java
@@ -1,30 +1,28 @@
 package org.firstinspires.ftc.team5385;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
-public class Tele_Op extends OpMode {
+@TeleOp(name = "TeleDrive", group = "Teleop")
+public class Tele_Op extends BigAutoBase {
 
     HardwareBot robot = null;
     CheesyDrive cheesyDrive = null;
 
     @Override
-    public void init() {
+    public void runOpMode() {
 
         robot = new HardwareBot();
         robot.init(hardwareMap);
         cheesyDrive = new CheesyDrive();
         robot.rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         robot.leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        waitForStart();
+        while(opModeIsActive()) {
 
-    }
-
-    @Override
-    public void loop() {
-
-        DriveSignal powers = cheesyDrive.cheesyDrive(gamepad1.left_stick_y, gamepad2.right_stick_x, false);
-        robot.rightDrive.setPower(powers.rightMotor);
-        robot.leftDrive.setPower(powers.leftMotor);
-
+            DriveSignal powers = cheesyDrive.cheesyDrive(gamepad1.left_stick_y*-1, gamepad1.right_stick_x, gamepad1.left_stick_y==0);
+            robot.rightDrive.setPower(powers.rightMotor);
+            robot.leftDrive.setPower(powers.leftMotor);
+        }
     }
 }
