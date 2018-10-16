d62c495c02ad851bbd80569c652692cec64ed2ab
Usman <mughani05@gmail.com>
2018-10-15 20:12:19 -0400

    Methods for lift

--- Team5385/src/main/java/org/firstinspires/ftc/team5385/HardwareLift.java
+++ Team5385/src/main/java/org/firstinspires/ftc/team5385/HardwareLift.java
@@ -6,6 +6,9 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class HardwareLift {    /* Public OpMode members. */
+    private static final double MAX_POT=3.3*(9300/9400);
+    private static final double MIN_POT=3.3*(100/9400);
+
     private DcMotor motor;
     private AnalogInput pot;
 
@@ -26,17 +29,31 @@ public class HardwareLift {    /* Public OpMode members. */
 
         motor.setPower(0);
 
-        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motor.setDirection(DcMotorSimple.Direction.REVERSE);
     }
-
-
 
     public void setMode(DcMotor.RunMode mode) {
         motor.setMode(mode);
     }
 
     public void setPower(double liftMotorPower) {
-        motor.setPower(liftMotorPower);
+        if ((getCurrentPosition() > MAX_POT && Math.signum(liftMotorPower) == 1) ||
+                (getCurrentPosition() < MIN_POT && Math.signum(liftMotorPower) == -1)){
+            motor.setPower(0);
+        }
+        else{
+            motor.setPower(liftMotorPower);
+        }
+
+    }
+
+    public double getCurrentPosition(){
+        return pot.getVoltage();
+    }
+
+    public double getError(double target){
+        return getCurrentPosition()-target;
+
     }
 }
