b04c5a65e65a6d33d0784b7af65ffd6f300e7fac
noshownoah <43425203+noshownoah@users.noreply.github.com>
2018-11-03 11:10:51 -0400

    worked on extension

--- Team5391/src/main/java/org/firstinspires/ftc/team5391/Teleop.java
+++ Team5391/src/main/java/org/firstinspires/ftc/team5391/Teleop.java
@@ -17,11 +17,34 @@ public class Teleop extends BaseOpMode {
         cheesyDrive = new CheesyDrive();
         drivetrain.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         while (opModeIsActive()) {
+
+            //moves lift and drivetrain
             DriveSignal powers = cheesyDrive.cheesyDrive(gamepad1.left_stick_y * -1, gamepad1.right_stick_x, false);
             drivetrain.setPower(powers.rightMotor, powers.leftMotor);
-            lift.setPower(gamepad2.right_stick_y);
+
+            //goes up with operator y/yellow
+            if(gamepad2.y) {
+                moveLift(8.1);
+            }
+            else if(gamepad2.a){
+                moveLift(.5);
+            }
+            else if(gamepad2.x) {
+                moveLift(7);
+            }
+
+            if (gamepad2.b) {
+                //collectInCrater();
+            }
+
+
+            //exstends out with operator triger/RT
+
+
+
             telemetry.addData("liftPosition: ",lift.getCurrentHeight());
             telemetry.update();
-            }
+
+        }
     }
 }
